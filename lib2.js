c0c72c47ffe03b225f4fa7ecb38df042300cd049
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-22 12:42:36 -0600

    Update lib2.js

--- lib2.js
+++ lib2.js
@@ -1,14 +1,12 @@
-// console.log('Bienvenido');
-
-var lienzo = document.getElementById("lienzo");
-var cd = lienzo.getContext("2d");
-
-function exhibirLineas(){
+window.exhibirLineas = function() {
+    var lienzo = document.getElementById("lienzo");
+    var cd = lienzo.getContext("2d");
     var x = 0;
-    while( x < 400){
+    while(x < 400){
+        cd.beginPath();
         cd.moveTo(x, 0);
-        cd.lineTo(400, 300-x);
+        cd.lineTo(400, 300 - x);
         cd.stroke();
-        x = x + 10;
+        x += 10;
     }
 }
