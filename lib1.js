ca1f4619736a4e00c80a88988b0b00eb72b5d268
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-28 04:40:30 +0000

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,36 +1,31 @@
 // lib.js
+// http://localhost/pwasd25/index.html?n=1&d=4
 const params = new URLSearchParams(window.location.search);
-const n = parseInt(params.get('n'));
-const d = parseInt(params.get('d'));
+const n = params.get('n');
+const d = params.get('d');
 
 class Quickchart {
-    constructor(n, d) {
-        this.n = n;
+    constructor(d) {
         this.d = d;
     }
-
     crearCadunos() {
-        const arr = [
-            ...Array(this.n).fill(1),
-            ...Array(this.d - this.n).fill(0)
-        ];
-        return arr.join(",");
+        let cadunos = "";
+        for(var i=1; i<this.d; i++) {
+            cadunos += "1,";
+        }
+        cadunos = cadunos.slice(0, -1);
+        return cadunos;
     }
-
     generarSrcImg() {
-        // Generamos colores: azul para llenos, gris claro para vacíos
-        const colores = [
-            ...Array(this.n).fill("4e79a7"), // azul
-            ...Array(this.d - this.n).fill("dddddd") // gris claro
-        ].join(",");
-
-        return "https://quickchart.io/chart?cht=p&chd=t:" + this.crearCadunos()
-            + "&chs=500x250"
-            + "&chl=" + this.n + "/" + this.d
-            + "&chco=" + colores;
+        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
+            + "&chs=500x250&chl=" + "1/" + this.d;
+        return url;
     }
 }
 
-let q = new Quickchart(n, d);
-document.getElementById("contenido").innerHTML =
-    '<img src="' + q.generarSrcImg() + '" />';
+const q = new Quickchart(d); error
+let q = new Quickchart(d);
+document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
+// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
+// q.generarSrcImg();
+// "<h1>Adios</h1>";
