5d1367ee83c6440fc10dd46c58a613216c04c440
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-27 22:18:23 -0600

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,27 +1,30 @@
-// lib.js
 const params = new URLSearchParams(window.location.search);
-const n = parseInt(params.get('n')); // número seleccionado
-const d = parseInt(params.get('d')); // total de divisiones
+const n = parseInt(params.get('n'));
+const d = parseInt(params.get('d'));
 
 class Quickchart {
-    constructor(n, d) {
-        this.n = n;
-        this.d = d;
+  constructor(d) {
+    this.d = d;
+  }
+
+  crearCadunos() {
+    let cadunos = "";
+    for (let i = 1; i < this.d; i++) {
+      cadunos += "1,";
     }
-    crearCadunos() {
-        let cadunos = "";
-        for (var i = 0; i < this.d; i++) {
-            cadunos += "1,";
-        }
-        cadunos = cadunos.slice(0, -1);
-        return cadunos;
-    }
-    generarSrcImg() {
-        let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-            + "&chs=500x250&chl=" + this.n + "/" + this.d;
-        return url;
-    }
+    cadunos += "1"; // agrega el último sin coma
+    return cadunos;
+  }
+
+  generarSrcImg() {
+    let url = "https://quickchart.io/chart?cht=p3&chd=t:" 
+      + this.crearCadunos() 
+      + "&chs=500x250&chl=" 
+      + "1/" + this.d;
+    return url;
+  }
 }
 
-let q = new Quickchart(n, d);
-document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
+let q = new Quickchart(d);
+document.getElementById("contenido").innerHTML = 
+  '<img src="' + q.generarSrcImg() + '" />';
