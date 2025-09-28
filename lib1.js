6bc646acb17072d498b7a168f7b5ae69108c5989
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-28 05:12:24 +0000

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,8 +1,7 @@
-// lib.js
 // http://localhost/pwasd25/index.html?n=1&d=4
 const params = new URLSearchParams(window.location.search);
-const n = params.get('n');
-const d = params.get('d');
+const n = +params.get('n');
+const d = +params.get('d');
 
 class Quickchart {
     constructor(d) {
@@ -10,7 +9,7 @@ class Quickchart {
     }
     crearCadunos() {
         let cadunos = "";
-        for(var i=0; i<this.d; i++) {
+        for (let i = 0; i < this.d; i++) {
             cadunos += "1,";
         }
         cadunos = cadunos.slice(0, -1);
@@ -23,10 +22,6 @@ class Quickchart {
     }
 }
 
-// const q = new Quickchart(d); error
 let q = new Quickchart(d);
-document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
-// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
-// q.generarSrcImg();
-// "<h1>Adios</h1>";
-
+document.getElementById("contenido").innerHTML =
+    '<img src="' + q.generarSrcImg() + '" />';
