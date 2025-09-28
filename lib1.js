1e804d3ba97ae390740ec3508f13aa33c1dec3bd
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-27 22:45:37 -0600

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,31 +1,46 @@
 // lib.js
-// http://localhost/pwasd25/index.html?n=1&d=4
+// Ejemplo de URL: http://localhost/pwasd25/index.html?n=1&d=4
+
+// 1. Obtener parámetros de la URL
 const params = new URLSearchParams(window.location.search);
-const n = params.get('n');
-const d = params.get('d');
+
+// CORRECCIÓN CLAVE: Convertir los valores a números enteros (int) 
+// ya que URLSearchParams.get() devuelve strings.
+const n = parseInt(params.get('n'));
+const d = parseInt(params.get('d')); 
 
 class Quickchart {
     constructor(d) {
-        this.d = d;
+        // Aseguramos que 'd' es un número al ser almacenado.
+        this.d = d; 
     }
+    
+    // Este método crea una cadena de '1,1,1,...' para el parámetro chd=t:
     crearCadunos() {
         let cadunos = "";
-        for(var i=1; i<this.d; i++) {
+        // El bucle se ejecuta d-1 veces. Por ejemplo, si d=4, se ejecuta 3 veces (i=1, 2, 3).
+        for(var i=1; i < this.d; i++) {
             cadunos += "1,";
         }
+        
+        // Quitar la última coma extra.
         cadunos = cadunos.slice(0, -1);
         return cadunos;
     }
+    
+    // Este método genera la URL completa para la imagen.
     generarSrcImg() {
+        // La URL final será similar a: 
+        // https://quickchart.io/chart?cht=p3&chd=t:1,1,1&chs=500x250&chl=1/4 (si d=4)
         let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-            + "&chs=500x250&chl=" + "1/" + this.d;
+             + "&chs=500x250&chl=" + "1/" + this.d;
         return url;
     }
 }
 
-// const q = new Quickchart(d); error
+// 2. Crear una instancia de Quickchart con el valor numérico de 'd'
 let q = new Quickchart(d);
+
+// 3. Insertar la etiqueta <img> en el elemento con id="contenido"
+// **Nota:** Debes tener un elemento en tu HTML con id="contenido" (ej: <div id="contenido"></div>).
 document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
-// '<img src="https://quickchart.io/chart?cht=p3&chd=t:1,1,1,1&chs=500x250&chl=1/4">';
-// q.generarSrcImg();
-// "<h1>Adios</h1>";
