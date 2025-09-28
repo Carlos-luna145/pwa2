925281fc557135a77c2d3c5324a458e16428802c
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-28 05:14:18 +0000

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,27 +1,31 @@
 // http://localhost/pwasd25/index.html?n=1&d=4
 const params = new URLSearchParams(window.location.search);
-const n = +params.get('n');
-const d = +params.get('d');
+const n = +params.get('n');  // rebanadas destacadas
+const d = +params.get('d');  // total de rebanadas
 
 class Quickchart {
-    constructor(d) {
+    constructor(d, n) {
         this.d = d;
+        this.n = n;
     }
     crearCadunos() {
         let cadunos = "";
         for (let i = 0; i < this.d; i++) {
-            cadunos += "1,";
+            if (i < this.n) {
+                cadunos += "2,"; // rebanada marcada
+            } else {
+                cadunos += "1,"; // rebanada normal
+            }
         }
-        cadunos = cadunos.slice(0, -1);
-        return cadunos;
+        return cadunos.slice(0, -1);
     }
     generarSrcImg() {
         let url = "https://quickchart.io/chart?cht=p3&chd=t:" + this.crearCadunos()
-            + "&chs=500x250&chl=" + "1/" + this.d;
+            + "&chs=500x250&chl=" + this.n + "/" + this.d;
         return url;
     }
 }
 
-let q = new Quickchart(d);
+let q = new Quickchart(d, n);
 document.getElementById("contenido").innerHTML =
     '<img src="' + q.generarSrcImg() + '" />';
