910a965ff92b58c0ded1e7326f63fb35207825d5
Carlos-luna145 <utp0156624@alumno.utpuebla.edu.mx>
2025-09-28 05:25:48 +0000

    Update lib1.js

--- lib1.js
+++ lib1.js
@@ -1,41 +1,32 @@
-// http://localhost/pwasd25/index.html?n=2&d=5
+// Obtener parámetros de URL
 const params = new URLSearchParams(window.location.search);
-const n = +params.get('n');  // rebanadas marcadas
-const d = +params.get('d');  // total de rebanadas
+const n = parseInt(params.get('n')); // número de porciones resaltadas
+const d = parseInt(params.get('d')); // total de porciones
 
 class Quickchart {
-    constructor(d, n) {
-        this.d = d;
+    constructor(n, d) {
         this.n = n;
+        this.d = d;
     }
 
-    crearValores() {
-        // Todas las rebanadas tienen valor 1
-        return Array(this.d).fill(1).join(',');
-    }
-
-    crearEtiquetas() {
-        // Las primeras n rebanadas muestran n/d, el resto vacías
-        let etiquetas = [];
-        for (let i = 0; i < this.d; i++) {
-            if (i < this.n) {
-                etiquetas.push(`${this.n}/${this.d}`);
-            } else {
-                etiquetas.push(''); // rebanadas sin etiqueta
-            }
-        }
-        return etiquetas.join('|');
+    // Crear datos para Quickchart
+    crearCadunos() {
+        // La idea: n porciones resaltadas y d-n normales
+        let cad = Array(this.n).fill(1); // porciones resaltadas
+        cad = cad.concat(Array(this.d - this.n).fill(0)); // resto
+        return cad.join(',');
     }
 
     generarSrcImg() {
-        let url = "https://quickchart.io/chart?cht=p3"
-            + "&chd=t:" + this.crearValores()
-            + "&chs=500x250"
-            + "&chl=" + this.crearEtiquetas();
+        // Usamos "chco" para dar color a las porciones: rojo para resaltadas, gris para las demás
+        let colors = Array(this.n).fill("ff0000").concat(Array(this.d - this.n).fill("cccccc")).join('|');
+
+        let url = "https://quickchart.io/chart?cht=p&chd=t:" + this.crearCadunos() +
+                  "&chs=500x250&chl=" + this.n + "/" + this.d +
+                  "&chco=" + colors;
         return url;
     }
 }
 
-let q = new Quickchart(d, n);
-document.getElementById("contenido").innerHTML =
-    '<img src="' + q.generarSrcImg() + '" />';
+let q = new Quickchart(n, d);
+document.getElementById("contenido").innerHTML = '<img src="' + q.generarSrcImg() + '" />';
